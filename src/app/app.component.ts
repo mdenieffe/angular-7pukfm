4fea48941945138f5c22aa2717dcffd74c668d60
mdenieffe <mdenieffe@gmail.com>
2022-07-12 21:57:13 +0100

    Updated to check how event binding worked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular';
+
+  constructor() {}
+
+  start() {
+    console.log('started');
+  }
+
+  complete() {
+    console.log('complete');
+  }
 }
--- src/app/components/app-status/app-status.component.ts
+++ src/app/components/app-status/app-status.component.ts
@@ -15,4 +15,5 @@ export class AppStatusComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+
 }
